e37ec27de3bae3a0809600eca9d454ed01f29709
void <wordhh1@gmail.com>
2024-07-04 12:14:12 -0600

    Added CSetBlockCommand

--- src/main/java/com/thevoidblock/clientgive/commands/CSetBlockCommand.java
+++ src/main/java/com/thevoidblock/clientgive/commands/CSetBlockCommand.java
@@ -1,4 +1,40 @@
 package com.thevoidblock.clientgive.commands;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import dev.xpple.clientarguments.arguments.CItemArgument;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.block.BlockState;
+import net.minecraft.command.argument.BlockPosArgumentType;
+import net.minecraft.command.argument.BlockStateArgument;
+import net.minecraft.command.argument.BlockStateArgumentType;
+import net.minecraft.command.argument.PosArgument;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.math.BlockPos;
+
+import static com.thevoidblock.clientgive.ClientGive.CLIENT;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+
 public class CSetBlockCommand {
+    public static void register() {
+        ClientCommandRegistrationCallback.EVENT.register(
+                (dispatcher, registryAccess) -> dispatcher.register(
+                        literal("csetblock")
+                                .then(
+                                        argument("pos", BlockPosArgumentType.blockPos())
+                                                .then(
+                                                        argument("block", BlockStateArgumentType.blockState(registryAccess))
+                                                                .executes(CSetBlockCommand::setblock)
+                                ))));
+    }
+
+    private static int setblock(CommandContext<FabricClientCommandSource> context) {
+        BlockPos blockPos = context.getArgument("pos", PosArgument.class).toAbsoluteBlockPos((ServerCommandSource) context.getSource());
+        BlockState blockState = context.getArgument("block", BlockStateArgument.class).getBlockState();
+
+        CLIENT.world.setBlockState(blockPos, blockState);
+        return 1;
+    }
 }
