537daa0e1252a1a458344fddb3d586fe5c798498
void <wordhh1@gmail.com>
2024-07-05 15:57:29 -0600

    Updated BooleanToggles to use StringIdentifiable

--- src/main/java/com/thevoidblock/voidcommands/util/BooleanToggles.java
+++ src/main/java/com/thevoidblock/voidcommands/util/BooleanToggles.java
@@ -1,7 +1,20 @@
 package com.thevoidblock.voidcommands.util;
 
-public enum BooleanToggles {
-    ON,
-    OFF,
-    TOGGLE
+import net.minecraft.util.StringIdentifiable;
+
+public enum BooleanToggles implements StringIdentifiable {
+    ON("on"),
+    OFF("off"),
+    TOGGLE("toggle");
+
+    private final String id;
+
+    BooleanToggles(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String asString() {
+        return this.id;
+    }
 }
