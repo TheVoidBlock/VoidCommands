874632012528f73f794dbe8475e2d0cac6e8e193
void <wordhh1@gmail.com>
2024-07-12 15:21:10 -0600

    Added getrender command to the command registration

--- src/main/java/com/thevoidblock/voidcommands/VoidCommandsRegistration.java
+++ src/main/java/com/thevoidblock/voidcommands/VoidCommandsRegistration.java
@@ -1,6 +1,7 @@
 package com.thevoidblock.voidcommands;
 
 import com.thevoidblock.voidcommands.commands.VGetCommand;
+import com.thevoidblock.voidcommands.commands.VGetRenderCommand;
 import com.thevoidblock.voidcommands.commands.VGhostPlacementCommand;
 import com.thevoidblock.voidcommands.commands.VSetBlockCommand;
 
@@ -13,5 +14,6 @@ public class VoidCommandsRegistration {
         VGetCommand.register();
         VSetBlockCommand.register();
         VGhostPlacementCommand.register();
+        VGetRenderCommand.register();
     }
 }
