6a890c3c54fe56c6113b1271f557530184176c53
void <wordhh1@gmail.com>
2024-07-27 22:10:36 -0600

    Added a header to VGetRenderCommand

--- src/main/java/com/thevoidblock/voidcommands/commands/VGetRenderCommand.java
+++ src/main/java/com/thevoidblock/voidcommands/commands/VGetRenderCommand.java
@@ -1,11 +1,13 @@
 package com.thevoidblock.voidcommands.commands;
 
 import com.mojang.brigadier.context.CommandContext;
+import com.thevoidblock.voidcommands.VoidCommandsStyler;
 import com.thevoidblock.voidcommands.mixin.GameOptionsAccessor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.text.Text;
 
 import static com.thevoidblock.voidcommands.VoidCommands.*;
 import static com.thevoidblock.voidcommands.VoidCommandsStyler.formatKeyValue;
@@ -23,7 +25,8 @@ public class VGetRenderCommand {
     private static int execute(CommandContext<FabricClientCommandSource> context) {
         assert CLIENT.world != null;
         context.getSource().sendFeedback(
-                formatKeyValue(String.format("chat.%s.server_simulation_distance", MOD_ID), CLIENT.world.getSimulationDistance()).append("\n")
+                Text.translatable(String.format("chat.%s.render_info_header", MOD_ID)).formatted(VoidCommandsStyler.HEADER_FORMATTING).append("\n\n")
+                        .append(formatKeyValue(String.format("chat.%s.server_simulation_distance", MOD_ID), CLIENT.world.getSimulationDistance())).append("\n")
                         .append(formatKeyValue(String.format("chat.%s.server_view_distance", MOD_ID), ((GameOptionsAccessor)CLIENT.options).getServerViewDistance())).append("\n")
                         .append(formatKeyValue(String.format("chat.%s.client_view_distance", MOD_ID), CLIENT.options.getViewDistance().getValue())).append("\n")
                         .append(formatKeyValue(String.format("chat.%s.clamped_view_distance", MOD_ID), CLIENT.options.getClampedViewDistance()))
