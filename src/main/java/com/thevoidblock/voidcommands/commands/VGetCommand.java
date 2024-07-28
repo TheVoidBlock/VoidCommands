016b0af7d81bf8079d58b63aca66b0eb2f1cd7d1
void <wordhh1@gmail.com>
2024-07-27 22:41:28 -0600

    Added feedback for VGetCommand

--- src/main/java/com/thevoidblock/voidcommands/commands/VGetCommand.java
+++ src/main/java/com/thevoidblock/voidcommands/commands/VGetCommand.java
@@ -9,8 +9,11 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 
 import static com.thevoidblock.voidcommands.VoidCommands.CLIENT;
+import static com.thevoidblock.voidcommands.VoidCommands.MOD_ID;
+import static java.lang.String.format;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
@@ -34,7 +37,9 @@ public class VGetCommand {
 
     private static int getItem(CommandContext<FabricClientCommandSource> context) {
         try {
-            giveItem(CItemArgument.getItemStackArgument(context, "item").createStack(1, false));
+            giveItem(context,
+                    CItemArgument.getItemStackArgument(context, "item").createStack(1, false)
+            );
         } catch (CommandSyntaxException e) {
             throw new RuntimeException(e);
         }
@@ -43,15 +48,26 @@ public class VGetCommand {
 
     private static int getItemCount(CommandContext<FabricClientCommandSource> context) {
         try {
-            giveItem(CItemArgument.getItemStackArgument(context, "item").createStack(IntegerArgumentType.getInteger(context, "count"), false));
+            giveItem(context,
+                    CItemArgument.getItemStackArgument(context, "item").createStack(IntegerArgumentType.getInteger(context, "count"), false)
+            );
         } catch (CommandSyntaxException e) {
             throw new RuntimeException(e);
         }
         return 1;
     }
 
-    private static void giveItem(ItemStack item) {
+    private static void giveItem(CommandContext<FabricClientCommandSource> context, ItemStack item) {
+
+        context.getSource().sendFeedback(
+                Text.translatable(format("chat.%s.receive_item", MOD_ID)).append(" ")
+                        .append(Integer.toString(item.getCount())).append(" ")
+                        .append(item.toHoverableText())
+        );
+
         assert CLIENT.player != null;
         CLIENT.player.giveItemStack(item);
     }
+
+
 }
