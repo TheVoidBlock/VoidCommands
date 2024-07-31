df06decf0d7aaf9a65fe4052b827655a9e1fa304
void <wordhh1@gmail.com>
2024-07-30 19:35:43 -0600

    Added Vec3d formatting to VoidCommandsStyler

--- src/main/java/com/thevoidblock/voidcommands/VoidCommandsStyler.java
+++ src/main/java/com/thevoidblock/voidcommands/VoidCommandsStyler.java
@@ -3,6 +3,7 @@ package com.thevoidblock.voidcommands;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.Vec3d;
 
 public class VoidCommandsStyler {
 
@@ -16,6 +17,18 @@ public class VoidCommandsStyler {
                 Text.translatable(key).formatted(KEY_FORMATTING)
                         .append(Text.literal(": ").formatted(SEPERATOR_FORMATTING))
                         .append(Text.literal(value.toString()).formatted(NUMBER_FORMATTING))
+                ;
+    }
+
+    public static MutableText formatVec3d(Vec3d vector) {
+        return
+                Text.literal(Double.toString(vector.x)).formatted(NUMBER_FORMATTING)
+                        .append(Text.literal(", ").formatted(SEPERATOR_FORMATTING))
+                        .append(Text.literal(Double.toString(vector.x)).formatted(NUMBER_FORMATTING))
+                        .append(Text.literal(", ").formatted(SEPERATOR_FORMATTING))
+                        .append(Text.literal(Double.toString(vector.y)).formatted(NUMBER_FORMATTING))
+                        .append(Text.literal(", ").formatted(SEPERATOR_FORMATTING))
+                        .append(Text.literal(Double.toString(vector.z)).formatted(NUMBER_FORMATTING))
                 ;
     }
 }
