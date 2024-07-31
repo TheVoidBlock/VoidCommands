04f7f4079b1f563d56608a11570e08fd40d0daa4
void <wordhh1@gmail.com>
2024-07-30 19:28:55 -0600

    Made styling more dynamic

--- src/main/java/com/thevoidblock/voidcommands/VoidCommandsStyler.java
+++ src/main/java/com/thevoidblock/voidcommands/VoidCommandsStyler.java
@@ -7,12 +7,15 @@ import net.minecraft.util.Formatting;
 public class VoidCommandsStyler {
 
     public static final Formatting HEADER_FORMATTING = Formatting.WHITE;
+    public static final Formatting SEPERATOR_FORMATTING = Formatting.WHITE;
+    public static final Formatting NUMBER_FORMATTING = Formatting.GOLD;
+    public static final Formatting KEY_FORMATTING = Formatting.AQUA;
 
     public static <T extends Number> MutableText formatTranslatableNumber(String key, T value) {
         return
-                Text.translatable(key).formatted(Formatting.AQUA)
-                        .append(Text.literal(": ").formatted(Formatting.WHITE))
-                        .append(Text.literal(value.toString()).formatted(Formatting.GOLD))
+                Text.translatable(key).formatted(KEY_FORMATTING)
+                        .append(Text.literal(": ").formatted(SEPERATOR_FORMATTING))
+                        .append(Text.literal(value.toString()).formatted(NUMBER_FORMATTING))
                 ;
     }
 }
