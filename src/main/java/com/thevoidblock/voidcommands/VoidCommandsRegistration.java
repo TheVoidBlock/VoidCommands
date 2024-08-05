9188d3b0dd15ec9641c4e1094ef68c8870eef840
void <wordhh1@gmail.com>
2024-08-05 14:22:15 -0600

    Added VSummonCommand to command registration

--- src/main/java/com/thevoidblock/voidcommands/VoidCommandsRegistration.java
+++ src/main/java/com/thevoidblock/voidcommands/VoidCommandsRegistration.java
@@ -16,5 +16,6 @@ public class VoidCommandsRegistration {
         VGhostPlacementCommand.register();
         VGetRenderCommand.register();
         VGetLocationCommand.register();
+        VSummonCommand.register();
     }
 }
