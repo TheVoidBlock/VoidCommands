889c673992becc6d83690d9b4e510babf60f356a
void <wordhh1@gmail.com>
2024-08-05 18:04:00 -0600

    Added a selector for VGetLocationCommand

--- src/main/java/com/thevoidblock/voidcommands/commands/VGetLocationCommand.java
+++ src/main/java/com/thevoidblock/voidcommands/commands/VGetLocationCommand.java
@@ -1,8 +1,11 @@
 package com.thevoidblock.voidcommands.commands;
 
 import com.mojang.brigadier.context.CommandContext;
+import dev.xpple.clientarguments.arguments.CEntityArgument;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
@@ -11,31 +14,31 @@ import static com.thevoidblock.voidcommands.VoidCommands.CLIENT;
 import static com.thevoidblock.voidcommands.VoidCommands.MOD_ID;
 import static com.thevoidblock.voidcommands.VoidCommandsStyler.*;
 import static java.lang.String.format;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class VGetLocationCommand {
     public static void register() {
         ClientCommandRegistrationCallback.EVENT.register(
                 (dispatcher, registryAccess) -> dispatcher.register(
-                        literal("vgetlocation")
-                                .executes(VGetLocationCommand::execute)
+                        literal("vgetlocation").executes(
+                                context -> execute(context, context.getSource().getEntity())
+                        ).then(argument("entity", CEntityArgument.entity()).executes(
+                                context -> execute(context, CEntityArgument.getEntity(context, "entity"))
+                        ))
+
         ));
     }
 
-    private static int execute(CommandContext<FabricClientCommandSource> context) {
+    private static int execute(CommandContext<FabricClientCommandSource> context, Entity entity) {
 
-        assert CLIENT.world != null;
-        final Identifier dimension = CLIENT.world.getDimensionEntry().getKey().get().getValue();
+        final Identifier dimension = entity.getWorld().getDimensionEntry().getKey().get().getValue();
 
-        assert CLIENT.player != null;
+        assert entity.getDisplayName() != null;
         context.getSource().sendFeedback(
-                Text.translatable(format("chat.%s.current_location", MOD_ID)).formatted(HEADER_FORMATTING).append(": ")
+                ((MutableText)entity.getDisplayName()).formatted(HEADER_FORMATTING).append("'s ").append(Text.translatable(format("chat.%s.current_location", MOD_ID))).append(": ")
                         .append(formatPlayerPos(
-                                new Vec3d(
-                                        CLIENT.player.getPos().x,
-                                        CLIENT.player.getPos().y,
-                                        CLIENT.player.getPos().z
-                                )
+                                entity.getPos()
                         )).append(" ")
                         .append(Text.translatable(format("chat.%s.location_dimension_separator", MOD_ID)).formatted(SEPERATOR_FORMATTING)).append(" ")
                         .append(Text.literal(dimension.toString()).formatted(IDENTIFIER_FORMATTING))
