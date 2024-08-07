938873d373637b8b80f5a13d7ffd2a7926fc4b3f
void <wordhh1@gmail.com>
2024-08-06 20:53:12 -0600

    Added registration for VDataCommand

--- src/main/java/com/thevoidblock/voidcommands/VoidCommandsRegistration.java
+++ src/main/java/com/thevoidblock/voidcommands/VoidCommandsRegistration.java
@@ -18,5 +18,6 @@ public class VoidCommandsRegistration {
         VGetLocationCommand.register();
         VSummonCommand.register();
         VKillCommand.register();
+        VDataCommand.register();
     }
 }
