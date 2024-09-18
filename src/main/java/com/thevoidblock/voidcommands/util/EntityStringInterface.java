64548dc933c3bd7b053b56b705f7dda00e44dfd0
void <wordhh1@gmail.com>
2024-09-18 15:35:40 -0600

    Revert "Added EntityStringInterface"
    This reverts commit 4bf6cf699c8bec05027de7cc45a6243a84dae51a.

--- src/main/java/com/thevoidblock/voidcommands/util/EntityStringInterface.java
+++
@@ -1,9 +0,0 @@
-package com.thevoidblock.voidcommands.util;
-
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.entity.Entity;
-
-@FunctionalInterface
-public interface EntityStringInterface {
-    String getString(Entity entity) throws CommandSyntaxException;
-}
